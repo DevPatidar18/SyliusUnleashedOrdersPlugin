983206ff9fe35153c8592441b659451031a5fe78
patel <patelnirav9033@gmail.com>
2024-02-26 11:45:39 +0530

    some changes in command

--- src/EventListener/PluginInstallationListener.php
+++ src/EventListener/PluginInstallationListener.php
@@ -40,22 +40,6 @@ class PluginInstallationListener implements EventSubscriberInterface
 
    private function addRoutesToConfiguration()
 	{
-	    $routeFilePath = $this->projectDir . '/config/routes/unleashed_products_plugin.yaml';
-	    $routeFileContent = <<<YAML
-	# Routes for unleashed products plugin
-	forge_labs_uk_unleashed_product_shop:
-	  resource: "@ForgeLabsUkSyliusUnleashedProductsPlugin/Resources/config/shop_routing.yml"
-	  prefix: /{_locale}
-	  requirements:
-	      locale: ^[A-Za-z]{2,4}(([A-Za-z]{4}|[0-9]{3}))?(_([A-Za-z]{2}|[0-9]{3}))?$
-
-	forge_labs_uk_sylius_unleashed_product_admin:
-	  resource: "@ForgeLabsUkSyliusUnleashedProductsPlugin/Resources/config/admin_routing.yml"
-	  prefix: '/%sylius_admin.path_name%'
-	YAML;
-
-	    file_put_contents($routeFilePath, $routeFileContent);
-
 	    // Execute migrations:diff
 	    $diffProcess = new Process(['php', 'bin/console', 'doctrine:migrations:diff']);
 	    $diffProcess->run();
